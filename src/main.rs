93eff6148b853e53f07d68d8b41870f61f6d9506
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-27 14:47:03 +0300

    Implemented borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 //borrowing is passing references as function parameters
 fn main() {
     let s1 = String::from("hello!");
-    let len = s1.len();
+    let len = calculat_length(&s1);
     
     println!("The length of {} is {}", s1, len);
+}
+
+fn calculat_length(s: &String) -> usize {
+    let length = s.len();
+    length
 }
